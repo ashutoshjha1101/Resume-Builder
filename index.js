f6a86e77243c356c7ee21a9ab354008cbbeb3d74
ashutoshjha1101 <82307498+ashutoshjha1101@users.noreply.github.com>
2024-06-03 16:42:20 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,42 @@
+// javascript is a programing language for web development its not like java as when javascipt was built java was a big name and all want to add java in there name but also there is a big difference in java and jsscript java works on compilor and javascript work on interpretor 
+// earlier js was used as frontend but now it is used as all framework like backand
+//alert("Hello"); 
+//alert is a function ,hello is a message and ; is for end
+//go to inspect - console - and then type javascript
+//data types in javascript - boolean,number,string
+//to check data type of js - write typeof(number,string,true or false)
+//to store anything in memory while writing in javascript use variable
+//variable= var myName="ashutosh"; like this
+//var ek box banaega memory m us box ka naam h myName jiski value hogi Ashutosh
+//promt is use for taking input from user
+//var yourName= promt("enter your name");
+
+ /*var a=8;
+var b=3;
+console.log("a is "+ b);
+console.log("b is "+ a);  consol.log ka use karte h print karte time
+ "concactination" use for string example:- "hello"+"world";
+ to count lenght of a world we can simply use 
+ "ex;
+ var myName="ashutosh";
+ myName.length;       "
+ */
+
+//  generateCV
+function generateCV()
+{
+   console.log("generate cv"); 
+//    What is console log in JavaScript?
+//    The console.log() method outputs a message to the web console. The message may be a single string (with optional substitution values), or it may be any one or more JavaScript objects.
+ let nameField=document.getElementById("nameField").Value;
+
+let nameT1=document.getElementById('nameT1');
+
+nameT1.innerHTML = nameField;
+
+
+
+
+
+
+}
